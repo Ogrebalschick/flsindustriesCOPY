09b7efc4198fba0b7406bd0cd1698e5d8f8de864
Ogrebalschick <ilya.ya.2011@bk.ru>
2021-10-25 10:50:04 +0600

    копия сайта flsindustries.ru дессктоп версия

---
+++ script/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			document.location.href = "http://flsindustriescopy/form.html";
+		});
+		return false;
+	});
+
+});
+
+$(document).ready(function(){
+	//Скрыть PopUp при загрузке страницы    
+	PopUpHide();
+});
+//Функция отображения PopUp
+function PopUpShow(){
+	$("#popup1").show();
+}
+//Функция скрытия PopUp
+function PopUpHide(){
+	$("#popup1").hide();
+}
+
+$(document).ready(function(){
+	//Скрыть PopUp при загрузке страницы    
+	PopUpHide2();
+});
+//Функция отображения PopUp
+function PopUpShow2(){
+	$("#popup2").show();
+}
+//Функция скрытия PopUp
+function PopUpHide2(){
+	$("#popup2").hide();
+}
